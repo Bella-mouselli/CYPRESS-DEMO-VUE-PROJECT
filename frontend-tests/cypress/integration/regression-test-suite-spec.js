2e36eba485ff680f8011befe78a0e39eed0fa616
Isabell Mouselli <bellam95@hotmail.com>
2021-10-27 23:31:28 +0200

    created a .gitignore and test project

---
+++ frontend-tests/cypress/integration/regression-test-suite-spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+// en test suite
+describe('Regression test suite', function(){
+// ett test case
+    it('Perform valid login', function(){
+     cy.visit('http://localhost:3000/')
+
+    })
+
+})
